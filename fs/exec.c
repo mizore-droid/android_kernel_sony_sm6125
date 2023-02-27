422b146e250bd65514faf73298bc6417e99b331f
3zore <minrei728@gmail.com>
2023-02-27 17:44:59 +0900

    KernelSU

--- fs/exec.c
+++ fs/exec.c
@@ -1693,6 +1693,10 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+            void *envp, int *flags);
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1706,6 +1710,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
+    ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
